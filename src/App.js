c63422f954e6517d19bdb8cce90f2e2abed1abc6
lemstanislav5 <lemstanislav5@gmail.com>
2023-05-07 23:55:32 +0300

    12121

--- src/App.js
+++ src/App.js
@@ -3,25 +3,26 @@ const App = () => {
   const [sort, setSort] = useState( new Array(10).fill(null).map(() => (Math.floor(Math.random() * 98) + 2)));
 
   useEffect(() => {
+    let arr = sort;
+    console.log(arr)
     function bubbleSortConcept1(arr) {
-      let newArr = [];
       for (let j = arr.length - 1; j > 0; j--) {
         for (let i = 0; i < j; i++) {
           if (arr[i] > arr[i + 1]) {
-            newArr[i] = arr[i + 1];
-            newArr[i + 1] = arr[i];
-          } else {
-            newArr[i] = arr[i];
-            newArr[i + 1] = arr[i + 1];
+            let temp = arr[i];
+            arr[i] = arr[i + 1];
+            arr[i + 1] = temp;
           }
         }
       }
     }
-    console.log(bubbleSortConcept1(sort))
-    // setSort(bubbleSortConcept1(sort));
+    console.log(bubbleSortConcept1(arr))
+    setSort(bubbleSortConcept1(arr));
   }, [sort]);
 
   console.log('render App',  sort);
+
+  if (!sort) return null;
   return (
    <div>
       { sort.map((item, i) => {
