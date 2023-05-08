9b11ce1019b531a3c6bf39973a59a393156f2719
lemstanislav5 <lemstanislav5@gmail.com>
2023-05-08 08:09:03 +0300

    121212

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
 import { useState, useEffect } from "react";
 const App = () => {
-  const [sort, setSort] = useState( new Array(10).fill(null).map(() => (Math.floor(Math.random() * 98) + 2)));
+  const [sort, setSort] = useState(null);
 
   useEffect(() => {
-    if (!sort) return;
-    let arr = sort;
+    let arr =  new Array(10).fill(null).map(() => (Math.floor(Math.random() * 98) + 2));
     console.log('До сортировки: ', arr);
     function bubbleSortConcept1(arr) {
       for (let j = arr.length - 1; j > 0; j--) {
+        // каждый раз цикл соершает на 1 итерацию меньше, так как число j уменьшается 
         for (let i = 0; i < j; i++) {
           if (arr[i] > arr[i + 1]) {
             let temp = arr[i];
@@ -17,19 +17,21 @@ const App = () => {
         }
       }
     }
+    //! Правильное решение arr.sort((a, b) => b - a);
+    //! reduce
+    //! map
     bubbleSortConcept1(arr)
     setSort(arr);
-    console.log('После сортировки: ', arr, sort);
-  }, [sort]);
-
-  console.log('render App',  sort);
+    console.log('После сортировки: ', arr);
+  }, []);
 
   if (!sort) return null;
   return (
    <div>
-      { sort.map((item, i) => {
-        return <div key={i}>{item}</div>
-      })
+      { 
+        sort.map((item, i) => {
+          return <div key={i}>{item}</div>
+        })
       }
     </div>
   );
